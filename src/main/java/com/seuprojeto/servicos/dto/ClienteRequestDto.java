d6646b233084d02deb638a6086cfe1fb8685d38c
Fábio José Miranda Montenegro <fabiomirandamonte@gmail.com>
2026-04-27 11:52:44 -0300

    feature: implement bean validation and global exception handling

--- src/main/java/com/seuprojeto/servicos/dto/ClienteRequestDto.java
+++ src/main/java/com/seuprojeto/servicos/dto/ClienteRequestDto.java
@@ -1,14 +1,23 @@
 package com.seuprojeto.servicos.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 
 public record ClienteRequestDto(
         @NotBlank(message = "Nome é obrigatório")
+        @Size(min = 3, max = 100)
         String nome,
 
         @NotBlank(message = "Telefone é obrigatório")
+        @Pattern(
+                regexp = "^\\d{10,11}$",
+                message = "Telefone deve conter 10 ou 11 números"
+        )
         String telefone,
 
         @NotBlank(message = "Endereço é obrigatório")
+        @Size(min = 5, max = 150)
         String endereco
 ) {}
--- src/main/java/com/seuprojeto/servicos/security/SecurityConfig.java
+++ src/main/java/com/seuprojeto/servicos/security/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.seuprojeto.servicos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 
 @Configuration
 public class SecurityConfig {
@@ -23,16 +24,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
+                                "/v3/api-docs",
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtFilter, 
+                    UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
